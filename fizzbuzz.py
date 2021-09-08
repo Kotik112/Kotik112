0f5c96e2779ef22cc35d467c94dd3476f8245704
Kotik112 <88910492+Kotik112@users.noreply.github.com>
2021-09-08 11:44:25 +0200

    New files added

---
+++ fizzbuzz.py
@@ -0,0 +1,23 @@
+try:
+    n = int(input("Enter the number: "))
+    i = 1
+    for i in range(n+1):
+        if  ((i % 3) == 0) and ((i % 5) == 0):
+            print("FizzBuzz")
+            continue
+        elif (i % 5) == 0:
+            print("Buzz")
+            continue
+        elif (i % 3) == 0:
+            print("Fizz")
+            continue
+        else:
+            print(f"{i}")
+
+except ValueError as e:
+    print("The program strictly accepts integer inputs.")
+    print(e)
+except ZeroDivisionError as e:
+    print("Division by Zero")
+    print(e)
+    
