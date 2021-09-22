1df41e870b5dc461e427296ee9558282aefd615c
Kotik <88910492+Kotik112@users.noreply.github.com>
2021-09-22 15:20:29 +0200

    room.py added

---
+++ Escape Room/room.py
@@ -0,0 +1,28 @@
+class Room:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.door = True
+        self.way_out = False
+
+    def __repr__(self) -> str:
+        return f"Door: {self.door}\nWay out: {self.way_out}"
+
+class LivingRoom(Room):
+    def __init__(self, name, description) -> None:
+        super().__init__(name)        #Inherits all variables and methods from Room
+        #Inget syfte, testar subclass variabel
+        self.description = description
+        
+
+    def __repr__(self) -> str:
+        return f"Door: {self.door}\nWay out: {self.way_out}\nDescription: {self.description}"
+
+def main():
+    #Testing purposes
+    living_room = LivingRoom("Living Room", "You find a bed and a locked closet.")
+    print(living_room)
+
+    #print(help(LivingRoom))
+
+if __name__ == "__main__":
+    main()
